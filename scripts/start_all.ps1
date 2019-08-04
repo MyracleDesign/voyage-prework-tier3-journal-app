f22a037ec8b966853d55b76622182e32cb63bf3f
Max Weber <weber.max.1988@live.de>
2019-08-04 21:33:40 +1200

    Update start_all.ps1

--- scripts/start_all.ps1
+++ scripts/start_all.ps1
@@ -7,6 +7,6 @@ Write-Host "Startup Server..."
 $ServerFolder = Join-Path -Path (get-item (Get-ScriptDirectory)).Parent.FullName -ChildPath "server";
 Start-Process powershell -ArgumentList  "aqueduct serve" -WorkingDirectory $ServerFolder;
 
-Write-Host "Started IdentityService.App,..."
+Write-Host "Started Client,..."
 $ClientFolder = Join-Path -Path (get-item (Get-ScriptDirectory)).Parent.FullName -ChildPath "client";
 Start-Process powershell -ArgumentList  "webdev serve --auto restart" -WorkingDirectory $ClientFolder;
