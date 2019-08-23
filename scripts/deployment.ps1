d68b3acc1217c69719a03379cd31fbce65313b59
MyracleDesign <weber.max.1988@live.de>
2019-08-23 23:38:28 +1200

    updated the deployment file

--- scripts/deployment.ps1
+++ scripts/deployment.ps1
@@ -1,5 +1,8 @@
 Write-Host "Deploy Server"
 git subtree push --prefix server digital-journal-api master
 
+Write-Host "It could be that this needs to be executed on heroku"
+heroku run /app/dart-sdk/bin/pub global run aqueduct:aqueduct auth add-client --id com.myracledesign.flutter_web_digital_journal --secret flutter-digital-journal-secret --connect \$DATABASE_URL --app digital-journal-api
+
 Write-Host "Startup Client..."
 git subtree push --prefix client digital-journal-client master
